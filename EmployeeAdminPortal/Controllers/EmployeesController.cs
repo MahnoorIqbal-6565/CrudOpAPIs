72bacb3a2ae5da1404f6f739f618696edaa338c8
Mahnoor Iqbal <131045940+MahnoorIqbal-6565@users.noreply.github.com>
2025-01-22 12:20:11 +0500

    Update EmployeesController.cs

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models.Entities;
+using EmployeeAdminPortal.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace EmployeeAdminPortal.Controllers
 {
@@ -7,5 +11,110 @@ namespace EmployeeAdminPortal.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+
+        private readonly ApplicationDBContext dbContext;
+
+        public EmployeesController(ApplicationDBContext dbContext)
+
+        {
+            this.dbContext = dbContext;
+
+        }
+        [Route("~/api/GetEmployees")]
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var AllEmployees = dbContext.Employees.ToList();
+            return Ok(AllEmployees);
+        }
+
+        [Route("~/api/GetEmployeeId/{id:guid}")]
+        [HttpGet]
+        //[Route("{id:guid}")]
+        public IActionResult GetEmployeebyId(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee is null) {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+
+        }
+
+        [Route("~/api/UpdateEmployees/{id:guid}")]
+        [HttpPut]
+        public IActionResult UpdateEmployees(Guid id, [FromBody] UpdateEmployeesDto updateEmployeeDto)
+        {
+          
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+            
+                employee.Name = updateEmployeeDto.Name;
+                employee.Email = updateEmployeeDto.Email;
+                employee.PhoneNumber = updateEmployeeDto.PhoneNumber;
+                employee.Address = updateEmployeeDto.Address;
+                employee.City = updateEmployeeDto.City;
+
+                dbContext.SaveChanges();
+
+                return Ok(employee);
+            }
+        }
+
+
+
+        [Route("~/api/AddEmployees")]
+        [HttpPost]
+        public IActionResult AddEmployees([FromBody] AddEmployeesDto addEmployeesDto)
+        {
+            if (addEmployeesDto == null)
+            {
+                return BadRequest("Employee data is null.");
+            }
+
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeesDto.Name,
+                Email = addEmployeesDto.Email,
+                PhoneNumber = addEmployeesDto.PhoneNumber,
+                Address = addEmployeesDto.Address,
+                City = addEmployeesDto.City
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+
+
+
+
+        [HttpDelete]
+        [Route("~/api/DeleteEmployee/{id:guid}")]
+        public IActionResult DeleteEmployees(Guid id)
+            {
+                var employee = dbContext.Employees.Find(id);
+                if (employee is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    dbContext.Employees.Remove(employee);
+                    dbContext.SaveChanges();
+                    return Ok(employee);
+                }
+            }
+        }
     }
-}
+
+
