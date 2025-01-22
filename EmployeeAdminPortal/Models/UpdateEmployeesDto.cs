bd2884a016a8ab00d2339250f19d5ade229778dd
Mahnoor Iqbal <131045940+MahnoorIqbal-6565@users.noreply.github.com>
2025-01-22 12:23:53 +0500

    Add files via upload

---
+++ EmployeeAdminPortal/Models/UpdateEmployeesDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EmployeeAdminPortal.Models
+{
+    public class UpdateEmployeesDto
+    {
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string PhoneNumber { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? City { get; set; }
+    }
+}
+
